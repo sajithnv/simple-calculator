d35051b00686f48d2bc6e5b8e3297bd08df59fc2
sajithnv <sajisajithnv@gmail.com>
2021-01-12 06:31:33 -0800

    modified the file to print anything what you want and how many times u want

--- bca.py
+++ bca.py
@@ -1,6 +1,8 @@
 class abc:
-    def a1(n):
+    def a1(n,m):
         for _ in range(n):
-            print('processing....')
+            print(f'{m}')
 c=abc
-c.a1(int(input('range: ')))
+m=str(input('Enter a string: '))
+n=int(input('Range: '))
+c.a1(n,m)
